a1ad756a000528c3a31150f40d2d06239b615565
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-02-26 20:37:09 -0300

    Todos

--- src/notion_based_ai/notion_types/__init__.py
+++ src/notion_based_ai/notion_types/__init__.py
@@ -6,3 +6,12 @@ class Database(Enum):
     MONTHS = "months"
     CARDS = "cards"
     TYPES = "types"
+
+
+# TODO 1  Criar estrutura que tenha a descricao do banco de dados
+# Sera usado para que o agente possa identicar melhor a estrutura montada
+# Talvez um agente que monte essa descrição?
+# class Database:
+#     database: DatabaseEnum
+#     description: str
+#     id: str
