c11049c122baaa845df6813425830e73d1d0b85a
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-05-23 18:39:47 -0300

    Create income tool and adjustments

--- src/MARiA/tools/new_tools/create_new_income.py
+++ src/MARiA/tools/new_tools/create_new_income.py
@@ -9,9 +9,9 @@ from pydantic import create_model, Field
 from MARiA.tools.new_tools.tool_interface import ToolInterface
 from pydantic import PrivateAttr
 
-class CreateNewOutTransactionV2(BaseTool, ToolInterface):
-    name: str = "criar_nova_transacao_de_saida"
-    description: str = "Cria uma nova transação de saída com os dados fornecidos - se o usuário não fornecer nenhum parâmetro, é necessário perguntar."
+class CreateNewIncome(BaseTool, ToolInterface):
+    name: str = "criar_nova_transacao_de_entrada"
+    description: str = "Cria uma nova transação de entrada com os dados fornecidos - se o usuário não fornecer nenhum parâmetro, é necessário perguntar."
     args_schema: Type[BaseModel] = None
     _notion_user_data: NotionUserData = PrivateAttr()
 
@@ -24,7 +24,7 @@ class CreateNewOutTransactionV2(BaseTool, ToolInterface):
 
 
     @classmethod
-    async def instantiate_tool(cls, notion_user_data: NotionUserData) -> 'CreateNewOutTransactionV2':
+    async def instantiate_tool(cls, notion_user_data: NotionUserData) -> 'CreateNewIncome':
         user_data = await notion_user_data.get_user_base_data()
 
         from enum import Enum
@@ -32,44 +32,28 @@ class CreateNewOutTransactionV2(BaseTool, ToolInterface):
             "CardEnum",
             {card["Name"].upper(): card["Name"] for card in user_data.cards['data']},
         )
-        CategoriesEnum = Enum(
-            "CategoryEnum",
-            {category["Name"].upper(): category["Name"] for category in user_data.categories['data']},
-        )
-        MacroCategoriesEnum = Enum(
-            "macroCategoryEnum",
-            {macro["Name"].upper(): macro["Name"] for macro in user_data.macroCategories['data']},
-        )
         MonthsEnum = Enum(
             "MonthEnum",
             {month["Name"].upper(): month["Name"] for month in user_data.months['data']},
         )
 
         InputModel = create_model(
-            "CreateNewOutTransactionInputDynamic",
+            "CreateNewIncomeInputDynamic",
             name=(str, Field(..., description="Nome escolhido para a transação")),
             amount=(float, Field(..., description="Valor da transação")),
             date=(str, Field(..., description="Data ISO da transação")),
             hasPaid=(bool, Field(..., description="Se a transação foi paga ou não. O default é True")),
             card_or_account=(
                 CardEnum,
-                Field(..., description="Cartão ou conta utilizada na saída"),
-            ),
-            category=(
-                CategoriesEnum,
-                Field(..., description="Categoria que classifica essa saída"),
-            ),
-            macro_category=(
-                MacroCategoriesEnum,
-                Field(..., description="Categoria macro da saída"),
+                Field(..., description="Cartão ou conta utilizada na entrada"),
             ),
             month=(
                 MonthsEnum,
-                Field(..., description="Mês ou gestão em que ocorreu a transação"),
+                Field(..., description="Mês ou gestão em que ocorreu a entrada"),
             ),
         )
 
-        tool = CreateNewOutTransactionV2(notion_user_data=notion_user_data)
+        tool = CreateNewIncome(notion_user_data=notion_user_data)
         tool.args_schema = InputModel
         return tool
 
@@ -79,25 +63,19 @@ class CreateNewOutTransactionV2(BaseTool, ToolInterface):
             amount = parms['args']['amount']
             date = parms['args']['date']
             card_or_account = parms['args']['card_or_account']
-            category = parms['args']['category']
-            macro_category = parms['args']['macro_category']
             month = parms['args']['month']
             hasPaid = parms['args']['hasPaid'] if 'hasPaid' in parms['args'] else True
 
             month_id = await self._notion_user_data.get_data_id(UserDataTypes.MONTHS, month)
             card_id = await self._notion_user_data.get_data_id(UserDataTypes.CARDS, card_or_account)
-            category_id = await self._notion_user_data.get_data_id(UserDataTypes.CATEGORIES, category)
-            marco_category_id = await self._notion_user_data.get_data_id(UserDataTypes.MACRO_CATEGORIES, macro_category)
 
-            notion_access.create_out_transaction(
-                name,
-                month_id,
-                amount,
-                date,
-                card_id,
-                category_id,
-                marco_category_id,
-                hasPaid
+            notion_access.create_in_transaction(
+                name = name,
+                month_id = month_id,
+                amount = amount,
+                date = date,
+                card_id = card_id,
+                hasPaid = hasPaid,
             )
             return ToolMessage(
                 content="Criado com sucesso",
@@ -108,25 +86,3 @@ class CreateNewOutTransactionV2(BaseTool, ToolInterface):
                 content=f"Ocorreu um erro na execução. Verifique os dados e tente novamente. Segue o erro para ajudar a entender: {e}",
                 tool_call_id=parms['id'],
             )
-        
-if __name__ == "__main__":
-
-    import asyncio
-
-    async def test():
-        tool = await CreateNewOutTransactionV2.instantiate_tool(notion_user_data)
-        await tool.ainvoke(
-            {
-                'args': {
-                    'name':'Teste',
-                    'amount':400,
-                    'date':'2025-05-23',
-                    'card_or_account':'NuConta',
-                    'category':'Diversos',
-                    'macro_category':'Não Essencial',
-                    'month':'2025 - Maio',
-                }
-            },
-            {}
-        )
-    asyncio.run(test())
