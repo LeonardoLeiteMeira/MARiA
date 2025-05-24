5550a7eea733d0363c97e61dbb021fc46fddd9f7
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-05-24 16:14:42 -0300

    Use ai to creat methods for all notion types

--- src/MARiA/notion_repository/basic_property.py
+++ src/MARiA/notion_repository/basic_property.py
@@ -36,5 +36,94 @@ class BasicProperty:
             return None
         return created_by.get('name') or created_by.get('id')
     
+    def __status(self):
+        status = self.property.get('status')
+        if status is None:
+            return None
+        return status.get('name')
+
+    def __last_edited_by(self):
+        last_edited_by = self.property.get('last_edited_by')
+        if last_edited_by is None:
+            return None
+        return last_edited_by.get('name') or last_edited_by.get('id')
+
+    def __updatedBy(self):
+        updated_by = self.property.get('updatedBy')
+        if updated_by is None:
+            return None
+        return updated_by.get('name') or updated_by.get('id')
+
+    def __rich_text(self):
+        rich_text = self.property.get('rich_text', [])
+        if not rich_text:
+            return ''
+        return ''.join([item.get('plain_text', '') for item in rich_text])
+
+    def __multi_select(self):
+        multi_select = self.property.get('multi_select', [])
+        if not multi_select:
+            return []
+        return [item.get('name') for item in multi_select]
+
+    def __people(self):
+        people = self.property.get('people', [])
+        if not people:
+            return []
+        return [person.get('name') or person.get('id') for person in people]
+
+    def __files(self):
+        files = self.property.get('files', [])
+        if not files:
+            return []
+        result = []
+        for file in files:
+            if file['type'] == 'external':
+                url = file['external']['url']
+            elif file['type'] == 'file':
+                url = file['file']['url']
+            else:
+                url = None
+            result.append({'name': file.get('name'), 'url': url})
+        return result
+
+    def __checkbox(self):
+        return self.property.get('checkbox', False)
+
+    def __url(self):
+        return self.property.get('url')
+
+    def __email(self):
+        return self.property.get('email')
+
+    def __phone_number(self):
+        return self.property.get('phone_number')
+
+    def __rollup(self):
+        rollup = self.property.get('rollup')
+        if not rollup:
+            return None
+        rollup_type = rollup.get('type')
+        return rollup.get(rollup_type)
+
+    def __last_edited_time(self):
+        return self.property.get('last_edited_time')
+
+    def __unique_id(self):
+        unique_id = self.property.get('unique_id')
+        if not unique_id:
+            return None
+        return {'number': unique_id.get('number'), 'prefix': unique_id.get('prefix')}
+
+    def __verification(self):
+        verification = self.property.get('verification')
+        if not verification:
+            return None
+        result = {'state': verification.get('state')}
+        verified_by = verification.get('verified_by')
+        if verified_by:
+            result['verified_by'] = verified_by.get('name') or verified_by.get('id')
+        result['date'] = verification.get('date')
+        return result
 
 
