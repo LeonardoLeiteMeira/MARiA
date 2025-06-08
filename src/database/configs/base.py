45eb3b50900a90117750767d9902474aba14fa31
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-06-07 21:17:59 -0300

    Start use AsyncAttrs

--- src/database/configs/base.py
+++ src/database/configs/base.py
@@ -1,9 +1,10 @@
 from sqlalchemy.orm import attributes
+from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
 
-class BaseModel(Base):
+class BaseModel(Base, AsyncAttrs):
     __abstract__ = True
 
     def is_attr_loaded(self, attr_name: str) -> bool:
