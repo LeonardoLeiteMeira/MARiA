df2476e939fd04d3a9fab70aa60e31276dcffdec
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-06-14 16:39:36 -0300

    Add database seeding for default user

--- src/domain/user_domain.py
+++ src/domain/user_domain.py
@@ -12,7 +12,7 @@ class UserDomain:
     
     async def get_user_valid_thread(self, user_id: str) -> list[ThreadModel]:
         return await self.__user_repository.get_user_valid_threads_by_user_id(user_id, self.__valid_thread_period)
-    
+
     async def create_new_user_thread(self, user_id: str) -> ThreadModel:
         new_thread = ThreadModel(
             id=uuid.uuid4(),
@@ -21,4 +21,21 @@ class UserDomain:
             updated_at=datetime.now() - timedelta(hours=-3),
         )
         return await self.__user_repository.create_user_new_thread(new_thread)
+
+    async def create_user(
+        self,
+        name: str,
+        phone_number: str,
+        email: str | None = None,
+    ) -> UserModel:
+        new_user = UserModel(
+            id=uuid.uuid4(),
+            name=name,
+            email=email,
+            phone_number=phone_number,
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
+        await self.__user_repository.create_user(new_user)
+        return new_user
 
