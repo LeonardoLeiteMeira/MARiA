92cd7a6480858350977f743ba3e44761ddeaada0
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-06-25 09:19:46 -0300

    Adjustment to send auth link to user

--- src/application/message_application.py
+++ src/application/message_application.py
@@ -1,6 +1,10 @@
 from domain import UserDomain
 from MARiA import MariaInteraction
 from messaging import MessageService
+from repository import UserModel
+from config import get_settings
+
+settings = get_settings()
 
 class MessageApplication:
     def __init__(self, user_domain: UserDomain, maria: MariaInteraction, message_service: MessageService):
@@ -22,6 +26,26 @@ class MessageApplication:
         if not user:
             await self.__message_service.send_message(chat_id, 'Desculpe! Mas a MARiA ainda nao esta atendendo!')
             return
+        
+        if not user.notion_authorization:
+            self.send_auth_link(user, chat_id)
 
         answer = await self.__maria.get_maria_answer(user, message)
         await self.__message_service.send_message(chat_id, answer)
+
+    async def send_auth_link(self, user: UserModel, chat_id: str):
+        link = self.__get_auth_link(user.id)
+        name = user.name
+        await self.__message_service.send_message(
+            chat_id,
+            f"Olá {name}! Para a MARiA começar a te atender preciso que você avise o Notion que permite ela acessar as informações. Acesse o link abaixo e selecione o sistema financeiro dentro do seu workspace. Quando finalizar so voltar aqui!\n\n{link}"
+        )
+
+        
+    def __get_auth_link(self, user_id: str) -> str:
+        client_id = settings.notion_client_id
+        redirect_uri = settings.notion_redirect_uri
+        response_type = "code"
+
+        base_url = f"https://www.notion.so/install-integration?response_type={response_type}&client_id={client_id}&redirect_uri={redirect_uri}&state={user_id}"
+        return base_url
