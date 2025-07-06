cd07c5e416d333e3d96d1ec25f7fec87efff5034
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-07-06 20:51:03 -0300

    Refactory main graph

--- src/MARiA/graph/state.py
+++ src/MARiA/graph/state.py
@@ -5,6 +5,3 @@ from langchain_core.messages import HumanMessage
 
 class State(TypedDict):
     messages: Annotated[list, add_messages]
-    final_Trial_messages: Annotated[list, add_messages]
-    is_trial: bool
-    user_input: HumanMessage
