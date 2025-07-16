a75f11c2107d2703803e619da4af3384209d3120
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-07-16 20:40:34 -0300

    Remove unnecessary files

--- src/MARiA/notion_repository/__init__.py
+++
@@ -1,2 +0,0 @@
-# from .notion_config import notion_access
-# from .notion_config import notion_user_data, NotionUserData
--- src/MARiA/tools/__init__.py
+++ src/MARiA/tools/__init__.py
@@ -1,17 +1,3 @@
-# from .search_transactions import SearchTransactions
-# from .get_transactions_categories import GetTransactionsCategories
-# from .create_new_transaction import CreateNewOutTransaction
-# from .get_transaction_types import GetTransactionTypes
-# from .get_months import GetMonths
-# from .get_user_cards import GetUserCards
-# from .get_data_structure import GetDataStructure
-# from .get_all_databases import GeAllDatabases
-# from .get_month_planning import GetMonthPlanning
-# from .register_feedback_and_email import RegisterFeedbackAndEmail
-# from .finished_test_period import FinishedTestPeriod
-# from .tool_agent.search_data import SearchData
-# from .tool_agent.write_data import WriteData
-
 from .new_tools.tool_interface import ToolInterface
 from .new_tools.create_card import CreateCard
 from .new_tools.create_new_income import CreateNewIncome
@@ -24,23 +10,3 @@ from .new_tools.read_user_base_data import ReadUserBaseData
 from .new_tools.get_plan_by_month import GetPlanByMonth
 from .new_tools.delete_data import DeleteData
 from .new_tools.get_month_data import GetMonthData
-
-# tools_to_read_data = [
-#     SearchTransactions(), 
-#     # GetTransactionsCategories(), 
-#     # GetTransactionTypes(), 
-#     # GetMonths(),
-#     # GetUserCards(), 
-#     GetDataStructure(), 
-#     GeAllDatabases(),
-#     GetMonthPlanning()
-# ]
-
-# tools_to_write_data = [
-#     CreateNewOutTransaction(), 
-# ]
-
-# websummitTools = [
-#     RegisterFeedbackAndEmail(Database()),
-#     FinishedTestPeriod(Database())
-# ]
