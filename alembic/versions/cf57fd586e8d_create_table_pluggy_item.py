09525340df979c8282cccc6751ac1bb563f97cc3
LeonardoLeiteMeira <leonardo.leitemeira10@gmail.com>
2025-08-03 19:11:23 -0300

    Create table to store pluggy item

---
+++ alembic/versions/cf57fd586e8d_create_table_pluggy_item.py
@@ -0,0 +1,43 @@
+"""create_table_pluggy_item
+
+Revision ID: cf57fd586e8d
+Revises: e879fb9293c8
+Create Date: 2025-08-03 18:40:38.638994
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import postgresql
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'cf57fd586e8d'
+down_revision: Union[str, None] = 'e879fb9293c8'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_table(
+        "pluggy_items",
+        sa.Column('id', sa.UUID(as_uuid=True), primary_key=True, nullable=False),
+        sa.Column('user_id', sa.UUID(as_uuid=True), sa.ForeignKey('users.id'), nullable=False),
+        sa.Column('item_id', sa.UUID(as_uuid=True), nullable=False),
+        sa.Column('connector', postgresql.JSONB, nullable=False),
+        sa.Column('products', sa.ARRAY(sa.String()), nullable=False),
+        sa.Column('executionStatus', sa.String(), nullable=False),
+        sa.Column('complementary_data', postgresql.JSONB, nullable=False),
+        sa.Column('status', sa.String(), nullable=False),
+        sa.Column('lastUpdatedAt', sa.DateTime(), nullable=False),
+        sa.Column('created_at', sa.DateTime(), nullable=False),
+        sa.Column('updated_at', sa.DateTime(), nullable=False),
+        sa.Column('connected_at', sa.DateTime(), nullable=False),
+    )
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_table("pluggy_items")
---
+++ src/repository/db_models/pluggy_item_model.py
@@ -0,0 +1,68 @@
+import uuid
+from datetime import datetime
+
+from sqlalchemy import String, DateTime, ForeignKey
+from sqlalchemy.dialects.postgresql import UUID, JSONB, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column
+
+from database.configs.base import BaseModel
+
+class UserItemModel(BaseModel):
+    __tablename__ = "pluggy_items"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        default=uuid.uuid4
+    )
+    user_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        ForeignKey("users.id"),
+        nullable=False
+    )
+    item_id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True),
+        nullable=False
+    )
+    connector: Mapped[dict] = mapped_column(
+        JSONB,
+        nullable=False
+    )
+    products: Mapped[list[str]] = mapped_column(
+        ARRAY(String),
+        nullable=False
+    )
+    complementary_data: Mapped[dict] = mapped_column(
+        JSONB,
+        nullable=False
+    )
+    executionStatus: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+    status: Mapped[str] = mapped_column(
+        String,
+        nullable=False
+    )
+    lastUpdatedAt: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+        onupdate=datetime.utcnow
+    )
+    connected_at: Mapped[datetime] = mapped_column(
+        DateTime,
+        nullable=False
+    )
+
+    def __repr__(self):
+        return f"<UserItemModel(id={self.id}, user_id={self.user_id}, status={self.status})>"
